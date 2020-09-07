60124028974d3250fbbadeee9b57b93d434d1185
KIYOUNGYI <yky2798@gmail.com>
2020-09-07 12:25:24 +0900

    큐,그래프 탐색

---
+++ src/com/study/bj/P1158.java
@@ -0,0 +1,23 @@
+package com.study.bj;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 요세푸스 문제
+ * https://www.acmicpc.net/problem/1158
+ * [1] 큐로 풀었다.
+ * [2] bfs 로도 가능한가?
+ * [3] 배열을 계속 이어붙여서 풀면 가능하긴 할 거 같은데 공간이 모든 케이스를 커버 칠 수 있는진 의문이다. 이걸 굳이 깊게 고민하면서 risk를 감수하고 싶진 않다.
+ * [4] 원형 연결리스트로?
+ */
+public class P1158
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/com/study/bj/P2667.java
@@ -0,0 +1,15 @@
+package com.study.bj;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * 단지번호 붙이기
+ * https://www.acmicpc.net/problem/2667
+ * [1] dfs 로 풀었음 (무난)
+ */
+public class P2667
+{
+
+}
