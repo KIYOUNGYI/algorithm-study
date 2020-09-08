747e764f89cce6bb976aed76cee7cf3e1904bd49
KIYOUNGYI <yky2798@gmail.com>
2020-09-08 14:00:27 +0900

    bfs 숨바꼭질

---
+++ src/com/study/bj/P1697.java
@@ -0,0 +1,18 @@
+package com.study.bj;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 숨바꼭질
+ * https://www.acmicpc.net/problem/1697
+ * 개삽질쓰
+ * 배열 별도로 하나 더 사용하진 않더라도
+ * 가지치기는 필요하더라,,, 안그러면 메모리 초과 에러 뜬다.
+ */
+public class P1697
+{
+
+
+}
