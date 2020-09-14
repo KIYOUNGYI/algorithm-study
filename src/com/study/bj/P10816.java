29f93832f42b9d2cf6299352548d9ed98f62ed82
liki <>
2020-09-14 23:44:16 +0900

    숫자 카드2

---
+++ src/com/study/bj/P10816.java
@@ -0,0 +1,14 @@
+package com.study.bj;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * 숫자 카드 2
+ * https://www.acmicpc.net/problem/10816
+ */
+public class P10816
+{
+    
+}
