1f9ef6046cb1a2bafdafe5d1058e4f8c9958f81a
liki <>
2020-09-14 22:46:19 +0900

    숫자 카드 (숫자놀이)

---
+++ src/com/study/bj/P10815.java
@@ -0,0 +1,19 @@
+package com.study.bj;
+
+import java.util.Scanner;
+
+/**
+ * 숫자카드
+ * https://www.acmicpc.net/problem/10815
+ *
+ * 난 이 문제 의외로 쉽게 풀었다.
+ * 근데 대부분 바이너리서치로 푸네?? 나도 바이너리로 다음에 풀어볼까?
+ * https://www.acmicpc.net/source/22534991
+ *
+ * 자바 binary search 소스
+ * https://www.acmicpc.net/source/22519215
+ */
+public class P10815
+{
+
+}
