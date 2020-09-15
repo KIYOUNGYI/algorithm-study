e841b14893809c5cd05f3dc6ce49052615945082
KIYOUNGYI <yky2798@gmail.com>
2020-09-15 15:47:15 +0900

    트리의 지름, 부분수열의 합

---
+++ src/com/study/bj/P1182.java
@@ -0,0 +1,18 @@
+package com.study.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *  부분수열의 합
+ *  https://www.acmicpc.net/problem/1182
+ *  조합 그 예전에 전수받았던 재귀 틀로 풀었다.
+ *  이거 근데 dfs로 풀수도 있는거 같다. 다음엔 dfs로 풀자
+ */
+public class P1182
+{
+
+}
---
+++ src/com/study/bj/P1967.java
@@ -0,0 +1,19 @@
+package com.study.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * 트리의 지름
+ * https://www.acmicpc.net/problem/1967
+ *
+ * dfs 로 풀었으니 다음은 bfs 로 풀어본다!
+ *
+ *
+ */
+public class P1967
+{
+}
+
