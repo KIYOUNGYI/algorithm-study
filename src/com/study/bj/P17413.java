f64c62357376d5eeca50a36849c7c34e2d938344
KIYOUNGYI <yky2798@gmail.com>
2020-10-03 15:17:09 +0900

    P17413 자료구조(스택) 단어 뒤집기 2

--- src/com/study/bj/P17413.java
+++ src/com/study/bj/P17413.java
@@ -5,27 +5,63 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 /**
  * 단어 뒤집기 2
  * https://www.acmicpc.net/problem/17413
- *
  */
-public class P17413
-{
+public class P17413 {
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     String input = br.readLine();
+    problem(input, bw);
+    bw.close();
+  }
 
-    String[] temp = input.split(">");
-    for(int i=0;i<temp.length;i++)
-    {
-      System.out.println(temp[i]);
+  public static void problem(String input, BufferedWriter bw) throws IOException {
+    Stack<Character> s = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+    int len = input.length();
+
+    for (int i = 0; i < len; i++) {
+      int start = i;
+      if (input.charAt(i) == '<') {
+        while (!s.isEmpty()) {
+          sb.append(s.pop());
+        }
+
+        StringBuilder temp = new StringBuilder();
+        start = i;
+        for (int j = start; j < input.length(); j++) {
+          temp.append(input.charAt(j));
+          if (input.charAt(j) == '>') {
+            i = j;
+            sb.append(temp);
+            break;
+          }
+        }
+        continue;
+      }
+
+      if (input.charAt(i) != ' ') {
+        s.push(input.charAt(i));
+      } else {
+        while (!s.isEmpty()) {
+          sb.append(s.pop());
+        }
+        sb.append(" ");
+      }
     }
 
+    while (!s.isEmpty()) {
+      sb.append(s.pop());
+    }
 
+    bw.write(sb.toString());
     bw.close();
   }
 }
