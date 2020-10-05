450034e20acb2f86e04da33580746266ca9b4d02
KIYOUNGYI <yky2798@gmail.com>
2020-10-05 12:06:33 +0900

    사탕게임 (브루트포스)

--- src/com/study/bj/P3085.java
+++ src/com/study/bj/P3085.java
@@ -1,110 +1,155 @@
 package com.study.bj;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
- * 시간초과
+ * 사탕게임
+ * https://www.acmicpc.net/problem/3085
  */
-public class P3085
-{
-  static int cnt2=0;
-  static int cnt1=0;
-  static int MAX=Integer.MIN_VALUE;
-  static int[] dx = {0,1,0,-1};
-  static int[] dy = {1,0,-1,0};
+public class P3085 {
 
-  static int[] seroMove1 = {0,0};
-  static int[] seroMove2 = {1,-1};
+  static int loopCnt = 0;
 
-  static int[] garoMove1 = {1,-1};
-  static int[] garoMove2 = {0,0};
+  static int MAX = Integer.MIN_VALUE;
+  static int[] dx = {0, 1, 0, -1};
+  static int[] dy = {1, 0, -1, 0};
 
   static int N;
   static char[][] arr;
-  static char[][] backup;
   static boolean[][] visit;
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    N = scan.nextInt();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    N = Integer.parseInt(br.readLine());
     arr = new char[N][N];
-    backup = new char[N][N];
     visit = new boolean[N][N];
 
-    scan.nextLine();
-    for(int i=0;i<N;i++)
-    {
-      String temp = scan.nextLine();
-      for(int j=0;j<N;j++)
-      {
+    for (int i = 0; i < N; i++) {
+      String temp = br.readLine();
+      for (int j = 0; j < N; j++) {
         arr[i][j] = temp.charAt(j);
-        backup[i][j]=arr[i][j];
+      }
+    }
+    candyGame();
+    bw.write(MAX + "\n");
+    bw.close();
+  }
+
+  public static void traverseAndPick(int x, int y) {
+    char temp = ' ';
+    for (int i = 0; i < 4; i++) {
+      int tx = x + dx[i];
+      int ty = y + dy[i];
+      if (tx >= 0 && ty >= 0 && tx < N && ty < N && arr[x][y] != arr[tx][ty]) {
+//        System.out.println("[기준 x,y:"+x+","+y+" 대상 tx,ty:"+tx+" "+ty+"] 루프 수:"+loopCnt++);
+        swap(x, y, tx, ty);
+//        System.out.println("swap");
+//        printArr();
+        calculate(tx, ty);
+        visit = new boolean[N][N];
+        swap(x, y, tx, ty);//recover
+//        System.out.println("recover");
+//        printArr();
+      }
+    }
+  }
+
+  public static void calculate(int tx, int ty) {
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        int ax = i;
+        int ay = j;
+        if (visit[ax][ay] == false) {
+          move(ax, ay);
+        }
       }
     }
 
-    for(int i=0;i<N;i++)
-    {
-      for(int j=0;j<N;j++)
-      {
-        swap_calculate_restore(i,j);
+  }
+
+  public static void move(int tx, int ty) {
+    int cnt1 = 0;
+    int cnt2 = 0;
+//    System.out.println("from tx:ty ["+tx+","+ty+"]");
+    char pivot = arr[tx][ty];
+    int localMax = 0;
+    for (int i = tx - 1; i >= 0; i--) {
+      loopCnt++;
+      if (arr[i][ty] != pivot) {
+        break;
+      }
+      visit[i][ty] = true;
+      cnt1 += 1;
+//      System.out.println("[x:"+i+" y:"+ty+"]");
+    }
+    for (int i = tx + 1; i < N; i++) {
+      loopCnt++;
+      if (arr[i][ty] != pivot) {
+        break;
+      }
+      visit[i][ty] = true;
+      cnt1 += 1;
+//      System.out.println("[x:"+i+" y:"+ty+"]");
+    }
+    cnt1 += 1;
+
+    for (int i = ty - 1; i >= 0; i--) {
+      loopCnt++;
+      if (arr[tx][i] != pivot) {
+        break;
+      }
+      visit[tx][i] = true;
+      cnt2 += 1;
+    }
+    for (int i = ty + 1; i < N; i++) {
+      loopCnt++;
+      if (arr[tx][i] != pivot) {
+        break;
+      }
+      visit[tx][i] = true;
+      cnt2 += 1;
+    }
+    cnt2 += 1;
+    localMax = max(cnt1, cnt2);
+//    System.out.println("local max:"+localMax);
+    if (MAX < localMax) {
+      MAX = localMax;
+    }
+  }
+
+
+  private static int max(int cnt1, int cnt2) {
+    return cnt1 >= cnt2 ? cnt1 : cnt2;
+  }
+
+  public static void swap(int x, int y, int tx, int ty) {
+    char temp = arr[tx][ty];
+    arr[tx][ty] = arr[x][y];
+    arr[x][y] = temp;
+  }
+
+
+  public static void candyGame() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        traverseAndPick(i, j);
       }
     }
-    System.out.println(MAX);
-    scan.close();
   }
 
-
-
-  public static int max(int x,int y)
-  {
-    if(x>=y)return x;
-    else return y;
-  }
-  private static void restore()
-  {
-    for(int i=0;i<N;i++)
-    {
-      for(int j=0;j<N;j++) { arr[i][j]=backup[i][j]; }
-    }
-  }
-
-  public static void swap_calculate_restore(int i,int j)
-  {
-    char temp1 = arr[i][j];
-    for(int a=0;a<4;a++)
-    {
-      int tx = i+dx[a];
-      int ty = j+dy[a];
-      if(tx>=0 && tx<N && ty>=0 && ty<N)
-      {
-        char temp2 = arr[tx][ty];
-        arr[tx][ty]=temp1;
-        arr[i][j]  =temp2;
-      }
-//      printArr();
-      // calculate();
-      restore();
-    }
-  }
-  public static void printArr()
-  {
-    for(int i=0;i<N;i++)
-    {
-      for(int j=0;j<N;j++)
-      {
+  public static void printArr() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
         System.out.print(arr[i][j]);
       }
       System.out.println();
     }
   }
-  public static void initV()
-  {
-    for(int i=0;i<N;i++)
-    {
-      for(int j=0;j<N;j++)
-      {
-        visit[i][j]=false;
-      }
-    }
-  }
-
 }
