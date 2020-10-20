7b17f487d96d720183b941fd4966c0101911edca
KIYOUNGYI <yky2798@gmail.com>
2020-10-20 16:03:05 +0900

    P10171

---
+++ src/com/study/bj/P10171.java
@@ -0,0 +1,11 @@
+package com.study.bj;
+
+public class P10171 {
+
+  public static void main(String[] args) {
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+  }
+}
