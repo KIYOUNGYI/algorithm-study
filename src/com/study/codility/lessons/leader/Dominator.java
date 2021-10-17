d7046d80e60c69e81f5260f9f1d9c564f54fd5ee
KIYOUNGYI <yky2798@gmail.com>
2021-10-17 22:42:34 +0900

    pdf 참고한 풀이 추가

--- src/com/study/codility/lessons/leader/Dominator.java
+++ src/com/study/codility/lessons/leader/Dominator.java
@@ -1,6 +1,7 @@
 package com.study.codility.lessons.leader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,11 +39,85 @@ public class Dominator {
     int[] b = {9, 3, 9, 3, 9, 7, 9};
     int[] c = {3, 4, 3, 2, 3, -1, 3, 3};
     int[] d = {0, 0, 1, 1, 1};
-    int[] e = {0, 0, 0, 1, 1, 1};
-    System.out.println(solution(d));
+    int[] e = {1, 2, 1};
+    int[] f = {0, 0, 1, 1, 1};
+    int[] g = {2, 1, 1, 1, 3};
+    System.out.println(solutionStudying(g));
+  }
+
+  /**
+   * [해냈다]
+   * <p>
+   * https://app.codility.com/demo/results/trainingFYHYUH-7VT/
+   *
+   *
+   * <p>
+   * [힘들다]
+   * <p>
+   * https://app.codility.com/demo/results/training29PYFF-UP8/
+   */
+  public static int solutionStudying(int[] arr) {
+
+    if (arr.length == 0) {
+      return -1;
+    }
+
+    if (arr.length == 1) {
+      return 0;
+    }
+
+    int[] brr = new int[arr.length];
+    for (int i = 0; i < brr.length; i++) {
+      brr[i] = arr[i];
+    }
+
+    Arrays.sort(arr);
+
+
+    int len = arr.length;
+    int leader = -1;
+
+    int candidate = arr[len / 2];
+
+    int temp = 0;
+    for (int i = 0; i < brr.length; i++) {
+      if (candidate == brr[i]) {
+        temp = i;
+        break;
+      }
+    }
+
+    int count = 1;
+
+    for (int i = len / 2 + 1; i < len; i++) {
+      if (arr[i] == candidate) {
+        count += 1;
+      } else {
+        break;
+      }
+    }
+
+    for (int i = len / 2 - 1; i >= 0; i--) {
+
+      if (arr[i] == candidate) {
+        count += 1;
+      } else {
+        break;
+      }
+
+    }
+
+    if (count > len / 2) {
+      leader = temp;
+    }
+
+    return leader;
   }
 
 
+  /**
+   * 다 맞긴 했는데, 단원이 제공하는 풀이를 활용해서 풀어봐야겠다.
+   */
   public static int solution(int[] arr) {
 
     Map<Integer, ArrayList<Integer>> map = new HashMap<>();
@@ -60,7 +135,7 @@ public class Dominator {
       }
     }
 
-    // System.out.println(map.toString());
+    System.out.println(map.toString());
 
     int size = 0;
     int idx = 0;
